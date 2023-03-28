daac5323cd73a08e11ee28b56c2072f14c58e79c
Ali Taghawi <marshaltaghawi@gmail.com>
2023-03-28 16:59:02 +0330

    creating card for blogs section

--- src/components/common/CardEL.js
+++ src/components/common/CardEL.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React from "react";
 
-const CardEL = () => {
+//MUI
+import {
+  Avatar,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Divider,
+  Typography,
+} from "@mui/material";
+
+const CardEL = ({ author, coverImage, id, slug, title }) => {
+  // console.log({author,coverImage, id, slug, title})
   return (
-    <div>
-      CardEL
-    </div>
+    <Card sx={{boxShadow: 'rgba(0,0,0,.2) 0px 5px 10px'}}>
+      <CardHeader
+        avatar={<Avatar alt={author.name} src={author.avatar.url} />}
+        title={
+          <Typography
+            component="p"
+            variant="p"
+            fontWeight={600}
+            color="text.secondary"
+            mr={1}
+          >
+            {author.name}
+          </Typography>
+        }
+      />
+      <CardMedia component='img' height='194' image={coverImage.url} />
+      <CardContent>
+        <Typography component='h3' variant="p" color='text.primary' fontWeight={600} >{title}</Typography>
+      </CardContent>
+      <Divider variant="middle" sx={{margin: '10px'}} />
+      <CardActions>
+        <Button variant="outlined" size="small" sx={{width: '100%'}} >
+          مطالعه مقاله
+        </Button>
+      </CardActions>
+    </Card>
   );
 };
 
